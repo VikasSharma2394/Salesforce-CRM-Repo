e15d14355d6a05f89340f0b24a00d46b96770388
VikasSharma <VikasSharma2394@gmail.com>
2023-06-15 15:25:31 +0530

    Error Removed

--- Selenium Project/src/ReadDataFromPDF.java
+++ Selenium Project/src/ReadDataFromPDF.java
@@ -1,16 +1,12 @@
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
 
 
 public class ReadDataFromPDF {
 public static void main(String[] args) throws IOException {
 	String text1 =null;
 	String path = "C:\\Users\\vikas.sharma\\Downloads\\SO-0000154 (1).pdf";
-	PDDocument doc = PDDocument.load(new File(path));
-	text1 = (new PDFTextStripper().getText(doc));
 	text1 = text1.trim().replaceAll("[\\t\\n\\r]+","");
 	text1 = text1.trim().replaceAll("  ", " ");
 	System.out.println(text1);
